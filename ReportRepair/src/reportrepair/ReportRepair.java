faa7b3c1f9630aaf6b3ac0d9e3faff1b5d23c83a
Prof.Bissallah Ekele <prof.bissallahekele@baeCapitan.local>
2020-12-01 16:33:17 +0000

    Created day1 folder

---
+++ ReportRepair/src/reportrepair/ReportRepair.java
@@ -0,0 +1,18 @@
+/*
+ */
+package reportrepair;
+
+/**
+ *
+ * @author bAe
+ */
+public class ReportRepair {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
